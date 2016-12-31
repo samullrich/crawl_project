4dbb50d693ec429becda0295684044889c2fc014
Sam Ullrich <Sam Ullrich>
2016-12-31 14:37:56 -0500

    These are files

---
+++ crawlIR.py
@@ -0,0 +1,21 @@
+class Crawler:
+    def __init__(self):
+        pass
+
+    def crawl(self,url,npages):
+        # Take a url
+        # Request the page using the requests library
+        # http://docs.python-requests.org/en/master/
+
+
+        # Go through and find the links
+        # BeautifulSoup Doc: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
+        pass
+
+    def normalizeURL(self,url):
+        pass
+
+
+if __name__ == '__main__':
+    print("Did you mean to do that?")
---
+++ notes.py
@@ -0,0 +1,5 @@
+# How to get python to execute
+# PATH="$PATH:C:\\Python34"
+
+# Add a file to git
+# git add crawlIR.py crawlerDB.py notes.py
